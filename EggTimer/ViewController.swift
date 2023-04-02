ccab6fcef4221f1e9e748d78dc55c4e80a536c00
İsmail Harmanda <iharmanda@hotmail.com>
2023-04-02 19:20:20 +0300

    move variables to in a dictionary and print via key

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,19 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime=5
-    let mediumTime=7
-    let hardTime=12
+    let eggTimes:[String:Int]=["Soft":5,"Medium":7,"Hard":12]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness=sender.currentTitle
-        
-            if(hardness=="Soft"){
-                print(softTime)
-            }else if(hardness=="Medium"){
-                print(mediumTime)
-        }else{
-            print(hardTime)
-        }
+        let hardness=sender.currentTitle!
+        print(eggTimes[hardness])
     }
 }
