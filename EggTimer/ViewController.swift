dfd54b24340348057b6b40c265c2eb71cc33f291
İsmail Harmanda <iharmanda@hotmail.com>
2023-04-04 01:59:05 +0300

    implement progress bar via percentage

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,29 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes:[String:Int]=["Soft":300,"Medium":420,"Hard":720]
-    
-    var secondsRemaining:Int=60
-    
+    let eggTimes:[String:Int]=["Soft":300,"Medium":420,"Hard":5]
     var timer=Timer()
+    var totalTime=0
+    var secondsPassed=0
     
     @IBOutlet weak var counterLabel: UILabel!
     
+    @IBOutlet weak var progressBar: UIProgressView!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        timer.invalidate()
         let hardness=sender.currentTitle!
-        secondsRemaining=eggTimes[hardness]!
-        
+        progressBar.progress=0
+        secondsPassed=0
+        counterLabel.text=hardness
+        timer.invalidate()
+        totalTime=eggTimes[hardness]!
         timer=Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
-        
-        }
+    }
+    
     @objc func updateTimer() {
-         if secondsRemaining>0 {
-             print("\(secondsRemaining) seconds.")
-             secondsRemaining-=1
-         }
-        if secondsRemaining<=0{ counterLabel.text="DONE"}else{timer.invalidate()
+        secondsPassed+=1
+        let percentageProgress=Float(secondsPassed)/Float(totalTime)
+        progressBar.progress=percentageProgress
+        if secondsPassed>=totalTime {
+            timer.invalidate()
             counterLabel.text="DONE!"
         }
     }
